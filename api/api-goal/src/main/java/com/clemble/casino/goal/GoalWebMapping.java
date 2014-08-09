cda72bd30778d486ee91f1d819afb37d35e20535
Anton O <mavarazy@gmail.com>
2014-08-09 07:51:34 +0300

    Changing me to my

--- api/api-goal/src/main/java/com/clemble/casino/goal/GoalWebMapping.java
+++ api/api-goal/src/main/java/com/clemble/casino/goal/GoalWebMapping.java
@@ -7,16 +7,16 @@ import com.clemble.casino.web.mapping.WebMapping;
  */
 public abstract class GoalWebMapping implements WebMapping {
 
-    final private static String GOAL_URL = "http://{host}//goal/track";
+    final private static String GOAL_URL = "http://{host}//goal";
     final public static String PLAYER_GOALS = "/{player}";
     final public static String PLAYER_GOALS_PENDING = "/{player}/pending";
     final public static String PLAYER_GOALS_REACHED = "/{player}/reached";
     final public static String PLAYER_GOALS_MISSED = "/{player}/missed";
 
-    final public static String MY_GOALS = "/me";
-    final public static String MY_GOALS_PENDING = "/me/pending";
-    final public static String MY_GOALS_REACHED = "/me/reached";
-    final public static String MY_GOALS_MISSED = "/me/missed";
+    final public static String MY_GOALS = "/my";
+    final public static String MY_GOALS_PENDING = "/my/pending";
+    final public static String MY_GOALS_REACHED = "/my/reached";
+    final public static String MY_GOALS_MISSED = "/my/missed";
 
     public static String toGoalUrl(String path) {
         return GOAL_URL + path;
