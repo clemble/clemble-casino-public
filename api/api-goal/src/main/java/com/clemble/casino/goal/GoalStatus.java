bffcad576dbd94d33f2413d11e90fa2c2c54a9c4
Anton O <mavarazy@gmail.com>
2014-08-16 11:28:54 +0300

    Update GoalStatus

--- api/api-goal/src/main/java/com/clemble/casino/goal/GoalStatus.java
+++ api/api-goal/src/main/java/com/clemble/casino/goal/GoalStatus.java
@@ -13,11 +13,6 @@ public class GoalStatus implements Comparable<GoalStatus> {
     final private String status;
     final private Date statusDate;
 
-    public GoalStatus(String status) {
-        this.status = status;
-        this.statusDate = new Date();
-    }
-
     @JsonCreator
     public GoalStatus(@JsonProperty("status") String status, @JsonProperty("statusDate") Date statusDate) {
         this.status = status;
@@ -55,5 +50,14 @@ public class GoalStatus implements Comparable<GoalStatus> {
     @Override
     public int compareTo(GoalStatus o) {
         return this.statusDate.compareTo(o.statusDate);
+    }
+
+    @Override
+    public String toString(){
+        return "goal:status:" + status + ":" + statusDate;
+    }
+
+    public static GoalStatus create(String status) {
+        return new GoalStatus(status, new Date());
     }
 }
