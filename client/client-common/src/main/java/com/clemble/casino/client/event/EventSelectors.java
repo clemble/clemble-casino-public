4ec469630a3ef9c990046ce806539854df7ddf46
Anton O <mavarazy@gmail.com>
2014-10-08 16:30:28 +0300

    Removing anonymous classes from EventSelectors

--- client/client-common/src/main/java/com/clemble/casino/client/event/EventSelectors.java
+++ client/client-common/src/main/java/com/clemble/casino/client/event/EventSelectors.java
@@ -2,48 +2,86 @@ package com.clemble.casino.client.event;
 
 import com.clemble.casino.event.Event;
 
-public class EventSelectors implements EventSelector {
-
-    final private EventSelector selector;
-
-    private EventSelectors(EventSelector selector) {
-        this.selector = selector;
-    }
+abstract public class EventSelectors implements EventSelector {
 
     public static EventSelectors where(EventSelector selector) {
-        return new EventSelectors(selector);
+        return new WhereEventSelector(selector);
     }
 
     public static EventSelectors not(final EventSelector selector) {
-        return new EventSelectors(new EventSelector() {
-            @Override
-            public boolean filter(Event event) {
-                return !selector.filter(event);
-            }
-        });
+        return new NotEventSelector(selector);
     }
 
     public EventSelectors and(final EventSelector eventSelector) {
-        return new EventSelectors(new EventSelector() {
-            @Override
-            public boolean filter(Event event) {
-                return selector.filter(event) && eventSelector.filter(event);
-            }
-        });
+        return new AndEventSelector(this, eventSelector);
     }
 
     public EventSelectors or(final EventSelector eventSelector) {
-        return new EventSelectors(new EventSelector() {
-            @Override
-            public boolean filter(Event event) {
-                return selector.filter(event) || eventSelector.filter(event);
-            }
-        });
+        return new OrEventSelector(this, eventSelector);
     }
 
-    @Override
-    public boolean filter(Event event) {
-        return selector != null ? selector.filter(event) : true;
+    public static class NotEventSelector extends EventSelectors {
+
+        final private EventSelector selector;
+
+        public NotEventSelector(EventSelector selector) {
+            this.selector = selector;
+        }
+
+        @Override
+        public boolean filter(Event event) {
+            return !selector.filter(event);
+        }
+
+    }
+
+    public static class AndEventSelector extends EventSelectors {
+
+        final private EventSelector selector;
+        final private EventSelector eventSelector;
+
+        public AndEventSelector(EventSelector selector, EventSelector eventSelector) {
+            this.selector = selector;
+            this.eventSelector = eventSelector;
+        }
+
+        @Override
+        public boolean filter(Event event) {
+            return selector.filter(event) && eventSelector.filter(event);
+        }
+
+    }
+
+    public static class WhereEventSelector extends EventSelectors {
+
+        final private EventSelector selector;
+
+        public WhereEventSelector(EventSelector selector) {
+            this.selector = selector;
+        }
+
+        @Override
+        public boolean filter(Event event) {
+            return selector != null ? selector.filter(event) : true;
+        }
+
+    }
+
+    public static class OrEventSelector extends EventSelectors {
+
+        final private EventSelector selector;
+        final private EventSelector eventSelector;
+
+        public OrEventSelector(EventSelector selector, EventSelector eventSelector) {
+            this.selector = selector;
+            this.eventSelector = eventSelector;
+        }
+
+        @Override
+        public boolean filter(Event event) {
+            return selector.filter(event) || eventSelector.filter(event);
+        }
+
     }
 
 }
