7c59d78e6f2898527f2a8e364c76a13ae2037d1c
mavarazy <mavarazy@gmail.com>
2014-10-15 23:39:19 +0300

    Merging PlayerAccountServiceContract & Service

--- api/api-payment/src/main/java/com/clemble/casino/payment/service/PlayerAccountService.java
+++ api/api-payment/src/main/java/com/clemble/casino/payment/service/PlayerAccountService.java
@@ -1,12 +1,21 @@
 package com.clemble.casino.payment.service;
 
+import com.clemble.casino.money.Currency;
 import com.clemble.casino.payment.PlayerAccount;
+
+import java.util.Collection;
+import java.util.List;
 
 /**
  * Created by mavarazy on 8/5/14.
  */
-public interface PlayerAccountService extends PlayerAccountServiceContract {
+public interface PlayerAccountService {
 
     public PlayerAccount myAccount();
+
+    public PlayerAccount getAccount(String playerWalletId);
+
+    public List<String> canAfford(Collection<String> players, Currency currency, Long amount);
+
 
 }
