0a0da96ed218907137bc8553ecbd9a5f59b0733d
mavarazy <mavarazy@gmail.com>
2014-10-17 19:34:18 +0300

    Refactoring GoalActionService

--- client/client-android/src/main/java/com/clemble/casino/android/game/AndroidGameActionTemplate.java
+++ client/client-android/src/main/java/com/clemble/casino/android/game/AndroidGameActionTemplate.java
@@ -3,17 +3,16 @@ package com.clemble.casino.android.game;
 import static com.clemble.casino.utils.Preconditions.checkNotNull;
 import static com.clemble.casino.game.GameWebMapping.*;
 
-import com.clemble.casino.event.Event;
 import com.clemble.casino.game.lifecycle.management.GameContext;
+import com.clemble.casino.game.lifecycle.management.service.GameActionService;
 import com.clemble.casino.lifecycle.management.event.action.Action;
 import org.springframework.web.client.RestTemplate;
 
 import com.clemble.casino.android.AbstractClembleCasinoOperations;
-import com.clemble.casino.client.game.ClientGameActionOperations;
 import com.clemble.casino.game.lifecycle.management.GameState;
 import com.clemble.casino.game.lifecycle.management.event.GameManagementEvent;
 
-public class AndroidGameActionTemplate extends AbstractClembleCasinoOperations implements ClientGameActionOperations {
+public class AndroidGameActionTemplate extends AbstractClembleCasinoOperations implements GameActionService {
 
     final private RestTemplate restTemplate;
 
--- client/client-common/src/main/java/com/clemble/casino/client/game/ClientGameActionOperations.java
+++
@@ -1,7 +0,0 @@
-package com.clemble.casino.client.game;
-
-import com.clemble.casino.game.lifecycle.management.service.GameActionService;
-
-public interface ClientGameActionOperations extends GameActionService {
-
-}
