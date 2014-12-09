68bac7f89b1dc97a249e6d1a2fd2c25a7600df40
Anton O <mavarazy@gmail.com>
2014-12-09 19:55:50 +0200

    Adding phone to the player phone

---
+++ api/api-common/src/main/java/com/clemble/casino/player/PlayerPhone.java
@@ -0,0 +1,39 @@
+package com.clemble.casino.player;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by mavarazy on 12/9/14.
+ */
+public class PlayerPhone {
+
+    final private String phone;
+
+    @JsonCreator
+    public PlayerPhone(@JsonProperty("phone") String phone) {
+        this.phone = phone;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        PlayerPhone that = (PlayerPhone) o;
+
+        if (!phone.equals(that.phone)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return phone.hashCode();
+    }
+
+}
--- api/api-player/src/main/java/com/clemble/casino/player/service/PlayerPhoneService.java
+++ api/api-player/src/main/java/com/clemble/casino/player/service/PlayerPhoneService.java
@@ -1,13 +1,14 @@
 package com.clemble.casino.player.service;
 
 import com.clemble.casino.ClembleService;
+import com.clemble.casino.player.PlayerPhone;
 
 /**
  * Created by mavarazy on 12/8/14.
  */
 public interface PlayerPhoneService extends ClembleService {
 
-    public boolean add(String phone);
+    public boolean add(PlayerPhone phone);
 
     public boolean remove();
 
