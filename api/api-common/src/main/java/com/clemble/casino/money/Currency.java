15ff1bfdf644fe53a6d714884ac6580515bb9e26
Anton O <mavarazy@gmail.com>
2015-01-19 22:57:54 +0200

    Adding victory and supporter to currency

--- api/api-common/src/main/java/com/clemble/casino/money/Currency.java
+++ api/api-common/src/main/java/com/clemble/casino/money/Currency.java
@@ -3,7 +3,9 @@ package com.clemble.casino.money;
 
 public enum Currency  {
 
-    FakeMoney;
+    FakeMoney,
+    Victory,
+    Supporter;
 
     final public static Currency DEFAULT = Currency.FakeMoney;
 }
