30be0abc575d563e9896abcff1d30bcb42a3c89f
Anton O <mavarazy@gmail.com>
2015-05-11 11:37:58 +0300

    Changing time processing

---
+++ api/api-common/src/main/java/com/clemble/casino/lifecycle/configuration/rule/timeout/MoveTimeoutCalculator.java
@@ -0,0 +1,13 @@
+package com.clemble.casino.lifecycle.configuration.rule.timeout;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+
+/**
+ * Created by mavarazy on 1/4/15.
+ */
+public interface MoveTimeoutCalculator extends TimeoutCalculator {
+
+}
